230af44fb382a5d2615041f7da5f7866c29930ff
siavash <siavash.kavousi.sk@gmail.com>
2016-05-05 19:13:12 +0430

    DeepConvNet: Implements conv - relu - pooling - conv - relu - pooling - fc - relu Net :)

--- DeepConvNet (MNIST)/deep_conv_net.py
+++ DeepConvNet (MNIST)/deep_conv_net.py
@@ -2,11 +2,6 @@ import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 from tensorflow.examples.tutorials.mnist import mnist
 
-# Loads training/validation/test sets
-mnist_data = input_data.read_data_sets('MNIST_data', one_hot=True)
-
-session = tf.InteractiveSession()
-
 
 def placeholder_inputs(batch_size):
     x = tf.placeholder(tf.float32, shape=[batch_size, mnist.IMAGE_PIXELS])
@@ -22,3 +17,39 @@ def weight_variable(shape):
 def bias_variable(shape):
     initial = tf.constant(0.1, shape=shape)
     return tf.Variable(initial)
+
+
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, [1, 1, 1, 1], 'SAME')
+
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, [1, 2, 2, 1], [1, 2, 2, 1], 'SAME')
+
+
+# Loads training/validation/test sets
+mnist_data = input_data.read_data_sets('MNIST_data', one_hot=True)
+session = tf.InteractiveSession()
+
+# First conv - relu - pooling layer
+W_conv1 = weight_variable([5, 5, 1, 32])
+b_conv1 = bias_variable([32])
+x, y = placeholder_inputs()
+x_image = tf.reshape(x, [-1, 28, 28, 1])
+
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
+h_pool1 = max_pool_2x2(h_conv1)
+
+# Second conv - relu - pooling layer
+W_conv2 = weight_variable([5, 5, 32, 64])
+b_conv2 = bias_variable([64])
+
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+h_pool2 = max_pool_2x2(h_conv2)
+
+# Third FC - relu layer
+W_fc1 = weight_variable([7 * 7 * 64, 1024])
+b_fc1 = bias_variable([1024])
+
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
